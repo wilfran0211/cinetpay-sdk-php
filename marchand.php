e22e55e76c083aca010fc2bf322ab8a4f7842382
wilfran0211 <159395740+wilfran0211@users.noreply.github.com>
2024-02-08 02:19:25 +0100

    Update marchand.php

--- marchand.php
+++ marchand.php
@@ -1,6 +1,6 @@
 <?php
 $marchand = array(
-    "apikey" => "", // Enrer votre apikey
-    "site_id" => "", //Entrer votre site_ID
-    "secret_key" => "" //Entrer votre clé secret
+    "apikey" => "", // 211303692565b0161a32f3d6.15840236
+    "site_id" => "", //5867657
+    "secret_key" => "" //171938546865b018ba875a57.90939108
 );
